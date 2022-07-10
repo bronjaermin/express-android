c342935b305e6144c8c4e9a7bdc8789ec5a79c59
bronjaermin <bronjaermin@gmail.com>
2022-07-11 00:24:32 +0200

    database changed

--- Database/mongoose.js
+++ Database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/exam-android")
+  .connect("mongodb+srv://bronjaermin:ilovemc788@cluster0.2zvuz.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("Database connected..."))
   .catch((e) => console.log("ERROR DATABASE"));
