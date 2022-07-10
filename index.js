436e4f396283b253bfe8cd9cf20b3e39c43d877e
bronjaermin <bronjaermin@gmail.com>
2022-07-11 00:33:38 +0200

    added .env

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("./Database/mongoose.js");
+require("dotenv").config();
 const cors = require("cors");
 
 const userRouter = require("./Routers/user.js");
 const adRouter = require("./Routers/ad.js");
 
 const app = express();
+const config = process.env;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("uploads"));
@@ -13,6 +15,6 @@ app.use(express.static("uploads"));
 app.use("/users", userRouter);
 app.use("/ads", adRouter);
 
-app.listen(3000, () => {
+app.listen(config.PORT || 3000, () => {
   console.log("Listening on port 3000...");
 });
